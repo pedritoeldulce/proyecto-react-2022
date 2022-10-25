60e4f5eadc0ceb36e469ddf6082244282a3c55ac
pedritoeldulce <franceskolyperez@gmail.com>
2022-10-25 00:28:08 -0500

    add variables de entorno and key

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 function App() {
 
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const[cryptos, setCryptos] = useState()
+
+  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCryptos(data.data)
+      })
+      .catch(() => {
+        console.error("La peticion fallò")
+      })
+  }, [])
+
+  if (!cryptos) return <span>Cargando ...</span>
+
   return (
-    <h1>Hola Noa</h1>
+    <>
+      <h1>Lista de criptomonedas</h1>
+        <ol>
+          {
+            cryptos.map(({id, name, priceUsd})  => (
+              <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+            ) )
+          }
+        </ol>
+    </>
   )
 }
 
